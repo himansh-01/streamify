2d2edb1b9734861335bdddd7d6fce0a942b97b8a
Himansh Prajapati <165716300+himansh-01@users.noreply.github.com>
2024-12-05 19:58:00 +0530

    Update Comment.js

--- src/store/Comment.js
+++ src/store/Comment.js
@@ -3,5 +3,15 @@ export const Comment = [
         message:"really good mvoie",
         id:1,
         person: "aggy-1"
+    },
+    {
+        message:"really good mvoie",
+        id:2,
+        person: "abc"
+    },
+    {
+        message:"really good mvoie",
+        id:3,
+        person: "xyz"
     }
 ]
