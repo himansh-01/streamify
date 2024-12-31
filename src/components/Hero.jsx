25dc459c76847c848134b715ff8bfc6470660773
himansh-01 <himanshprajapati2004@gmail.com>
2024-12-31 17:09:18 +0530

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 function Hero(props) {
   return (
     <div className='w-[80%] p-8 grid justify-self-center shadow-2xl bg-[#6c34af] mb-4'>
-        <div className='flex flex-wrap'>
-           <div className='w-[30%] flex'><img src={props.img} className='w-[15rem] h-[15rem]'/></div>
+        <div className='flex flex-wrap hero'>
+           <div className='mr-[3rem] flex'><img src={props.img} className='w-[15rem] h-[15rem]'/></div>
            <div className='w-[70%] text-xl self-center'>{props.name}</div>
         </div>
-        <div className='flex'>
+        <div className='flex hero-reverse'>
            <div className='w-[70%] text-xl self-center'>{props.name2}</div>
-           <div className='w-[30%]'><img src={props.img2} className='w-[15rem] h-[15rem]'/></div>
+           <div className=''><img src={props.img2} className='w-[15rem] h-[15rem]'/></div>
         </div>   
     </div>
   )
